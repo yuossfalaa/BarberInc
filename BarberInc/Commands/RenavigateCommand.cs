87cc3a99793fcfc97018c045e715ab708f461eb4
yuossfalaa <yuossfalla@gmail.com>
2024-04-26 01:59:11 +0300

    .

---
+++ BarberInc/Commands/RenavigateCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using BarberInc.States.Navigators;
+
+namespace BarberInc.Commands
+{
+    public class RenavigateCommand : ICommand
+    {
+        private readonly IRenavigator _renavigator;
+
+        public RenavigateCommand(IRenavigator renavigator)
+        {
+            _renavigator = renavigator;
+        }
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            _renavigator.Renavigate();
+        }
+    }
+}
