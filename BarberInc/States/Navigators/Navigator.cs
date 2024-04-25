6e7f42d2854e8ee1627e3e33ad43f9bdca6547d8
yuossfalaa <yuossfalla@gmail.com>
2024-04-26 01:59:41 +0300

    .

---
+++ BarberInc/States/Navigators/Navigator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarberInc.States.Navigators
+{
+    public class Navigator : INavigator
+    {
+        private BaseViewModel _currentViewModel;
+        public BaseViewModel CurrentViewModel
+        {
+            get
+            {
+                return _currentViewModel;
+            }
+            set
+            {
+                _currentViewModel?.Dispose();
+                _currentViewModel = value;
+                StateChanged?.Invoke();
+
+            }
+        }
+        public event Action StateChanged;
+
+    }
+}
