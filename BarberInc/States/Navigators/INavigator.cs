83d4be4637328c130dbcfd683248bc8d82983e33
yuossfalaa <yuossfalla@gmail.com>
2024-04-26 01:59:34 +0300

    .

---
+++ BarberInc/States/Navigators/INavigator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarberInc.States.Navigators
+{
+    public interface INavigator
+    {
+        public enum ViewType
+        {
+            Login,
+            SignUp,
+            Home,
+            Reservation
+        }
+        BaseViewModel CurrentViewModel { get; set; }
+        event Action StateChanged;
+    }
+}
