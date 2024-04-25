d6ad2fef401bff624945298bfe04a05776f8297e
yuossfalaa <yuossfalla@gmail.com>
2024-04-26 01:59:26 +0300

    .

---
+++ BarberInc/HostBuilders/AddViewModelsHostBuilderExtension.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BarberInc.Factories;
+using BarberInc.States.Navigators;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace BarberInc.HostBuilders
+{
+    public static class AddViewModelsHostBuilderExtension
+    {
+        public static IHostBuilder AddViewModels(this IHostBuilder host)
+        {
+            host.ConfigureServices(services =>
+            {
+
+                #region ViewModels
+                services.AddTransient<MainViewModel>();
+                services.AddTransient<HomeViewModel>();
+                services.AddTransient<LoginViewModel>();
+                services.AddTransient<SignUpViewModel>();
+                services.AddTransient<ReservationViewModel>();
+
+                #endregion
+
+                #region CreateViewModel
+                services.AddSingleton<CreateViewModel<HomeViewModel>>(services => () => services.GetRequiredService<HomeViewModel>());
+                services.AddSingleton<CreateViewModel<LoginViewModel>>(services => () => services.GetRequiredService<LoginViewModel>());
+                services.AddSingleton<CreateViewModel<SignUpViewModel>>(services => () => services.GetRequiredService<SignUpViewModel>());
+                services.AddSingleton<CreateViewModel<ReservationViewModel>>(services => () => services.GetRequiredService<ReservationViewModel>());
+
+                #endregion
+
+                #region ViewModelDelegateRenavigator
+                services.AddSingleton<IViewModelFactory, ViewModelFactory>();
+                services.AddSingleton<ViewModelDelegateRenavigator<HomeViewModel>>();
+                services.AddSingleton<ViewModelDelegateRenavigator<LoginViewModel>>();
+                services.AddSingleton<ViewModelDelegateRenavigator<SignUpViewModel>>();
+                services.AddSingleton<ViewModelDelegateRenavigator<ReservationViewModel>>();
+
+                #endregion
+
+            });
+            return host;
+        }
+    }
+}
