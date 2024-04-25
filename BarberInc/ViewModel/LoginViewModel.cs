1e41fca751aadf607b863617eccb1e9c51f97fe2
yuossfalaa <yuossfalla@gmail.com>
2024-04-26 01:59:56 +0300

    .

---
+++ BarberInc/ViewModel/LoginViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using BarberInc.Commands;
+using BarberInc.States.Navigators;
+
+namespace BarberInc.ViewModel
+{
+    public class LoginViewModel:BaseViewModel
+    {
+        private readonly IRenavigator signupRenavigat;
+        public ICommand SignUpRenavigatCommand { get; set; }
+
+        public LoginViewModel(ViewModelDelegateRenavigator<SignUpViewModel> signupRenavigat)
+        {
+            this.signupRenavigat = signupRenavigat;
+            SignUpRenavigatCommand = new RenavigateCommand(this.signupRenavigat);
+        }
+    }
+}
