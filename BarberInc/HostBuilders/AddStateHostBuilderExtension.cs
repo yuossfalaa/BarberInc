6f2ccc41419d47fd6e8c4c1384e832f3d361f2d0
yuossfalaa <yuossfalla@gmail.com>
2024-04-26 01:59:23 +0300

    .

---
+++ BarberInc/HostBuilders/AddStateHostBuilderExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BarberInc.States.Navigators;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace BarberInc.HostBuilders
+{
+    public static class AddStateHostBuilderExtension
+    {
+        public static IHostBuilder AddState(this IHostBuilder host)
+        {
+            host.ConfigureServices(services =>
+            {
+                services.AddSingleton<INavigator, Navigator>();
+ 
+
+            });
+            return host;
+        }
+    }
+}
