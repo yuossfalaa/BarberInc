7eac1b1ce0102b014a7ca44663212c59ef35e23b
yuossfalaa <yuossfalla@gmail.com>
2024-04-26 01:59:50 +0300

    .

---
+++ BarberInc/ViewModel/BaseViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+
+namespace BarberInc.ViewModel
+{
+    public delegate TViewModel CreateViewModel<TViewModel>() where TViewModel : BaseViewModel;
+    public partial class BaseViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        Visibility isBusy = Visibility.Collapsed;
+        [ObservableProperty]
+        int font_1 = 12;
+        [ObservableProperty]
+        int font_2 = 16;
+        [ObservableProperty]
+        int font_2_5 = 18;
+        [ObservableProperty]
+        int font_3 = 22;
+        [ObservableProperty]
+        int font_4 = 28;
+        [ObservableProperty]
+        int font_5 = 32;
+        [ObservableProperty]
+        int font_6 = 36;
+        [ObservableProperty]
+        int headerFont = 42;
+        [ObservableProperty]
+        int border = 2;
+        public virtual void Dispose()
+        {
+
+        }
+    }
+}
