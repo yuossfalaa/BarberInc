9ff9881b16356bb454efb7585e13a3e51c339fb6
yuossfalaa <yuossfalla@gmail.com>
2024-04-26 02:00:35 +0300

    .

--- BarberInc/App.xaml.cs
+++ BarberInc/App.xaml.cs
@@ -4,6 +4,9 @@ global using UserControl = System.Windows.Controls.UserControl;
 using System.Configuration;
 using System.Data;
 using System.Windows;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using BarberInc.HostBuilders;
 
 namespace BarberInc
 {
@@ -12,6 +15,37 @@ namespace BarberInc
     /// </summary>
     public partial class App : Application
     {
+        private readonly IHost _host;
+        public App()
+        {
+ 
+            try
+            {
+                _host = CreateHostBuilder().Build();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public static IHostBuilder CreateHostBuilder(string[] args = null)
+        {
+            return Host
+                .CreateDefaultBuilder(args)
+                .AddViewModels()
+                .AddViews()
+                .AddState()
+                .UseDefaultServiceProvider(options =>
+                    options.ValidateScopes = false);
+        }
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            await _host.StartAsync();
+            MainWindow = _host.Services.GetRequiredService<MainWindow>();
+            MainWindow.Show();
+            base.OnStartup(e);
+        }
     }
-
 }
+
+
