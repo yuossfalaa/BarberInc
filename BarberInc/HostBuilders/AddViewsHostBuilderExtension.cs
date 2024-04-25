c93e770e427eae4ea6f557330b82a8fe652c9809
yuossfalaa <yuossfalla@gmail.com>
2024-04-26 01:59:29 +0300

    .

---
+++ BarberInc/HostBuilders/AddViewsHostBuilderExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BarberInc.Views;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace BarberInc.HostBuilders
+{ 
+        static class AddViewsHostBuilderExtension
+        {
+            public static IHostBuilder AddViews(this IHostBuilder host)
+            {
+                host.ConfigureServices(services =>
+                {
+                    services.AddSingleton<MainWindow>();
+                });
+                return host;
+            }
+        }
+}
