32e0277747e7b08aebc9fbe5ac225ff4e08146dc
yuossfalaa <yuossfalla@gmail.com>
2024-04-26 01:59:17 +0300

    .

---
+++ BarberInc/Factories/IViewModelFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BarberInc.States.Navigators;
+
+namespace BarberInc.Factories
+{
+
+    public interface IViewModelFactory
+    {
+        BaseViewModel CreateViewModel(INavigator.ViewType viewType);
+    }
+}
