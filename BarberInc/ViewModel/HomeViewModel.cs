95ea6dd0e87d9d74d647117836e6bb7c24d6ac74
yuossfalaa <yuossfalla@gmail.com>
2024-05-03 19:01:48 +0300

    .

--- BarberInc/ViewModel/HomeViewModel.cs
+++ BarberInc/ViewModel/HomeViewModel.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using BarberInc.Commands;
+using BarberInc.States.Navigators;
+using CommunityToolkit.Mvvm.Input;
 
 namespace BarberInc.ViewModel
 {
-    public class HomeViewModel:BaseViewModel
+    public partial class HomeViewModel:BaseViewModel
     {
+        private readonly IRenavigator LoginRenave;
+        private readonly IRenavigator ReservationRenave;
+
+        public ICommand ReservationRenavigatCommand { get; set; }
+
+        public HomeViewModel(ViewModelDelegateRenavigator<LoginViewModel> loginRenave, ViewModelDelegateRenavigator<ReservationViewModel> reservationRenave)
+        {
+            LoginRenave = loginRenave;
+            ReservationRenave = reservationRenave;
+            ReservationRenavigatCommand = new RenavigateCommand(ReservationRenave);
+        }
+        [RelayCommand]
+        void Logout ()
+        {
+            RenavigateCommand renavigateCommand = new RenavigateCommand(LoginRenave);
+            renavigateCommand.Execute(null);
+        }
+
+
     }
 }
