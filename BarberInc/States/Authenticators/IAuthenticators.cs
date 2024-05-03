b9957b5b321aee96aebc8f7310aabd489ae7bce7
yuossfalaa <yuossfalla@gmail.com>
2024-05-03 19:01:45 +0300

    .

---
+++ BarberInc/States/Authenticators/IAuthenticators.cs
@@ -0,0 +1,18 @@
+﻿using Domain;
+using Services.DomainServices.UserServices;
+
+namespace BarberInc.States.Authenticators
+{
+    public interface IAuthenticators
+    {
+        User CurrentUser { get; set; }
+        bool IsLoggedIn { get; }
+
+        event Action StateChanged;
+
+        Task ChangePassword(User user, string NewPassword);
+        Task Login(string UserName, string Password);
+        void Logout();
+        Task<IAuthenticationService.RegistrationResult> Register(User inRegistrationUser);
+    }
+}
