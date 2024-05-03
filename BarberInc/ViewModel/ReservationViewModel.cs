c1f50b42df795453bafda9f111b989c73e332321
yuossfalaa <yuossfalla@gmail.com>
2024-05-03 19:01:57 +0300

    .

--- BarberInc/ViewModel/ReservationViewModel.cs
+++ BarberInc/ViewModel/ReservationViewModel.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using BarberInc.Commands;
+using BarberInc.States.Navigators;
 
 namespace BarberInc.ViewModel
 {
     public class ReservationViewModel:BaseViewModel
     {
+        private readonly IRenavigator homeRenavigat;
+        public ICommand HomeRenavigatCommand { get; set; }
+
+        public ReservationViewModel(ViewModelDelegateRenavigator<HomeViewModel> homeRenavigat)
+        {
+            this.homeRenavigat = homeRenavigat;
+            HomeRenavigatCommand = new RenavigateCommand(homeRenavigat);
+        }
+
+
     }
 }
