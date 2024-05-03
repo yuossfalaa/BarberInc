95b7a717e27a89e8aaa274fb7fb10319bfc9c765
yuossfalaa <yuossfalla@gmail.com>
2024-05-03 19:01:11 +0300

    .

---
+++ BarberInc/HostBuilders/AddServicesHostBuilderExtension.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MaterialDesignThemes.Wpf;
+using Microsoft.AspNet.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Services.DomainServices.UserServices;
+
+namespace BarberInc.HostBuilders
+{
+    public static class AddServicesHostBuilderExtension
+    {
+        public static IHostBuilder AddServices(this IHostBuilder host)
+        {
+
+            host.ConfigureServices((context, services) =>
+            {
+                services.AddSingleton<IPasswordHasher, PasswordHasher>();
+                services.AddSingleton<IUserService, UserDataService>();
+                services.AddSingleton<IReservationService, ReservationDataService>();
+                services.AddSingleton<IAuthenticationService, AuthenticationService>();
+                services.AddSingleton<ISnackbarMessageQueue, SnackbarMessageQueue>();
+            });
+            return host;
+        }
+    }
+}
+
+
