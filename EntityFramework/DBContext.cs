0890a48b6180ca4536c51884e25892be9dfa1d04
yuossfalaa <yuossfalla@gmail.com>
2024-05-04 18:10:01 +0300

    .

--- EntityFramework/DBContext.cs
+++ EntityFramework/DBContext.cs
@@ -11,6 +11,7 @@ namespace EntityFramework
     public class DBContext : DbContext
     {
         public DbSet<User> Users { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
         public DBContext(DbContextOptions options) : base(options) { }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
