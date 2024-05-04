8c32b5b198285949b088f92b256b897c200b53d5
yuossfalaa <yuossfalla@gmail.com>
2024-05-05 00:05:46 +0300

    .

---
+++ BarberInc/ViewModel/AdminDashViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace BarberInc.ViewModel
+{
+    public partial class AdminDashViewModel : BaseViewModel
+    {
+        [ObservableProperty]
+        AdminUserPanalViewModel adminUserPanalViewModel;
+        [ObservableProperty]
+        AdminReservationsPanalViewModel adminReservationsPanalViewModel;
+
+        public AdminDashViewModel(AdminReservationsPanalViewModel adminReservationsPanalViewModel, AdminUserPanalViewModel adminUserPanalViewModel)
+        {
+            AdminReservationsPanalViewModel = adminReservationsPanalViewModel;
+            AdminUserPanalViewModel = adminUserPanalViewModel;
+        }
+    }
+}
