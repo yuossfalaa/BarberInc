997309f633efef73347bd7c9d6d80d1a74e1a533
yuossfalaa <yuossfalla@gmail.com>
2024-05-04 18:09:27 +0300

    .

---
+++ BarberInc/Converters/ViewModelToVisibilityConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows;
+
+namespace BarberInc.Converters
+{
+    public class CurrentViewModelToVisibilityConverter : IValueConverter
+    {
+        public Visibility TrueValue { get; set; }
+        public Visibility FalseValue { get; set; }
+
+        public CurrentViewModelToVisibilityConverter()
+        {
+            FalseValue = Visibility.Collapsed;
+            TrueValue = Visibility.Visible;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return TrueValue;
+            }
+            else if (value.ToString() == typeof(LoginViewModel).ToString()|| value.ToString() == typeof(SignUpViewModel).ToString())
+            {
+                return FalseValue;
+            }
+            else
+            {
+                return TrueValue;
+
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
