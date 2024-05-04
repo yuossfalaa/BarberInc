fa35222bc744f5387de34d9da5a4990e897e2be4
yuossfalaa <yuossfalla@gmail.com>
2024-05-05 00:04:26 +0300

    .

---
+++ BarberInc/Converters/DateTimeConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace BarberInc.Converters
+{
+    public class DateTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is DateTime)
+            {
+                DateTime date = (DateTime)value;
+                return date.ToString("D");
+            }
+            else
+            {
+                return value;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
