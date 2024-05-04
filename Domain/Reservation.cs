ff35aedfeae8e1e3eae7b02752e3fc4a71f38ea7
yuossfalaa <yuossfalla@gmail.com>
2024-05-04 18:09:52 +0300

    .

--- Domain/Reservation.cs
+++ Domain/Reservation.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace Domain
 {
@@ -14,13 +15,46 @@ namespace Domain
         HaircutKids,
         Lineup,
     }
-    public class Reservation:DomainObject
+    public class Reservation : DomainObject
     {
-        public string FullName { get; set; } = "";
-        public string PhoneNumber { get; set; } = "";
-        public DateTime Date { get; set; } = DateTime.Now;
-        public ServiceType Service { get; set; } = ServiceType.Haircut;
-        public int UserId {  get; set; }
-        
+
+        private string _FullName = "";
+
+        public string FullName
+        {
+            get { return _FullName; }
+            set { _FullName = value; OnPropertyChanged(nameof(FullName)); }
+        }
+
+
+        private string _PhoneNumber = "";
+
+        public string PhoneNumber
+        {
+            get { return _PhoneNumber; }
+            set { _PhoneNumber = value; OnPropertyChanged(nameof(PhoneNumber)); }
+        }
+
+
+        private DateTime _Date = DateTime.Now;
+
+        public DateTime Date
+        {
+            get { return _Date; }
+            set { _Date = value; OnPropertyChanged(nameof(Date)); }
+        }
+
+
+        private ServiceType _Service = ServiceType.Haircut;
+
+        public ServiceType Service
+        {
+            get { return _Service; }
+            set { _Service = value; OnPropertyChanged(nameof(Service)); }
+        }
+
+        public int? UserId { get; set; }
+        public User? User { get; set; }
+
     }
 }
