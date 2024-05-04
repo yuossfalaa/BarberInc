ac86fb441f987fa5417d44d6f829cb4920a2d461
yuossfalaa <yuossfalla@gmail.com>
2024-05-05 00:06:44 +0300

    .

--- Services/DomainServices/UserServices/IUserService.cs
+++ Services/DomainServices/UserServices/IUserService.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Domain;
+﻿using Domain;
 
 namespace Services.DomainServices.UserServices
 {
     public interface IUserService : IDataService<User>
     {
         Task<User> GetByEmail(string Email);
+        Task<List<User>> Search(string term);
 
     }
 }
